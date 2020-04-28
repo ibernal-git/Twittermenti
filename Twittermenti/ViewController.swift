c620eb0f627a935e6f949ddf3025dac8816a69b4
Imanol Bernal <ibernal@icloud.com>
2020-04-28 13:51:45 +0200

    Initial commit

---
+++ Twittermenti/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Twittermenti
+//
+//  Created by Imanol Bernal on 28/04/2020.
+//  Copyright © 2020 Imanol Bernal. All rights reserved.
+//
+
+import UIKit
+import SwifteriOS
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var backgroundView: UIView!
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var sentimentLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    @IBAction func predictPressed(_ sender: Any) {
+    
+    
+    }
+    
+}
+
