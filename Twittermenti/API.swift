9f58378ec78b446f6a57fea6a5637bc998589f5f
Imanol Bernal <ibernal@icloud.com>
2020-04-28 15:47:15 +0200

    Add load of API Keys

---
+++ Twittermenti/API.swift
@@ -0,0 +1,15 @@
+//
+//  API.swift
+//  Twittermenti
+//
+//  Created by Imanol Bernal on 28/04/2020.
+//  Copyright © 2020 Imanol bernal. All rights reserved.
+//
+
+import Foundation
+
+
+class API: Codable {
+    var Key: String = ""
+    var Secret: String = ""
+}
--- Twittermenti/ViewController.swift
+++ Twittermenti/ViewController.swift
@@ -15,14 +15,35 @@ class ViewController: UIViewController {
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var sentimentLabel: UILabel!
     
+    private var apiKey = API()
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadAPI()
+        let swifter = Swifter(consumerKey: apiKey.Key, consumerSecret: apiKey.Secret)
         
     }
 
     @IBAction func predictPressed(_ sender: Any) {
     
     
+    }
+    
+    func loadAPI() {
+        
+        let path = Bundle.main.url(forResource: "Secrets", withExtension: "plist")
+        if let data = try? Data(contentsOf: path!) {
+            let decoder = PropertyListDecoder()
+            do {
+                apiKey = try decoder.decode(API.self, from: data)
+                //print(apiKey.Key)
+            } catch {
+                print("Error dencoding API. \(error)")
+            }
+        }
+        
     }
     
 }
